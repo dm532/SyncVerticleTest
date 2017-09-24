896f6176fb9f90c514af1985409cbeadc458af5d
dm532 <dm532@example.com>
2017-09-24 23:14:47 +0530

    Test

--- PushServerVerticle.java
+++ PushServerVerticle.java
@@ -2,8 +2,9 @@ package com.sw.ff.platform.server.push.websockets.verticles;
 
 
 import java.util.Date;
-import java.util.Map.Entry;
+import java.util.Set;
 
+import co.paralleluniverse.fibers.Suspendable;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Handler;
@@ -12,38 +13,59 @@ import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
 import io.vertx.core.shareddata.LocalMap;
 import io.vertx.core.spi.BufferFactory;
+import io.vertx.ext.sync.SyncVerticle;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.ErrorHandler;
 import io.vertx.ext.web.handler.StaticHandler;
-import io.vertx.ext.web.handler.sockjs.BridgeEventType;
 import io.vertx.ext.web.handler.sockjs.BridgeOptions;
 import io.vertx.ext.web.handler.sockjs.PermittedOptions;
 import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+//import io.vertx.ext.web.handler.sockjs.BridgeEvent;
 import io.vertx.ext.web.handler.sockjs.SockJSSocket;
+import com.sw.ff.platform.server.push.websockets.oms.handler.WebsocketServerBridgeEventHandler;
 
 
-public class PushServerVerticle extends AbstractVerticle {
+public class PushServerVerticle extends SyncVerticle {
 
     private static final Logger logger = LoggerFactory.getLogger(PushServerVerticle.class);
 
     private SockJSHandler sockJSHandler;
     
+    
+    public PushServerVerticle() {
+    		
+    }
+    
 	@Override
+	@Suspendable
 	public void start() {
 
-
-        try {
-        		for(int i = 0; i < 5; i++) {
-					vertx.deployVerticle(new SyncVerticleTest(false));
-        		}
-        		for(int i = 0; i < 5; i++) {
-					vertx.deployVerticle(new SyncVerticleTest(true));
-        		}
-		} catch (Exception e1) {
-			logger.error(e1);
-			e1.printStackTrace();
-			logger.info(e1);
-		}
+		createWebsocketHandler();
+		vertx.deployVerticle(new BasicVerticle(), new DeploymentOptions().setWorker(true));
+		Router router = Router.router(vertx);
+		router.route("/eventbus/*").handler(sockJSHandler);
+		router.route().failureHandler(errorHandler());
+		router.route().handler(staticHandler());
+		vertx.createHttpServer().requestHandler(router::accept).listen(8080);
+		
 	}
-}
 
+	
+	
+    private SockJSHandler createWebsocketHandler() {
+        BridgeOptions options = new BridgeOptions()
+                .addOutboundPermitted(new PermittedOptions().setAddressRegex(""+ "auction\\.[0-9]+"));
+        sockJSHandler = SockJSHandler.create(vertx).bridge(options, new BridgeEventHandler(vertx));
+        return sockJSHandler;
+    }
+
+
+    private ErrorHandler errorHandler() {
+        return ErrorHandler.create(true);
+    }
+
+    private StaticHandler staticHandler() {
+        return StaticHandler.create()
+            .setCachingEnabled(false);
+    }
+}
